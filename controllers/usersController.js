e1a8a88b91425d0de492c247dd5dcdbd80913118
moods-1 <ceemoods@gmail.com>
2023-01-25 08:52:40 -0500

    Add Cloudinary for image storage.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const User = require('../models/User');
 const { tryCatch } = require('../utilities/tryCatch');
 const { OK, SUCCESS } = require('../helpers/constants');
@@ -5,6 +6,13 @@ const {
 	responseFormatter,
 	responseCacher,
 } = require('../helpers/helperFunctions');
+const { v2 } = require('cloudinary');
+
+v2.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 exports.getAllUsersController = tryCatch(async (req, res, next) => {
 	const result = await User.find();
@@ -21,6 +29,9 @@ exports.getUserByIdController = tryCatch(async (req, res) => {
 
 exports.postUserController = tryCatch(async (req, res) => {
 	const { body } = req;
+	const { image } = body;
+	const imageUrl = await v2.uploader.upload(image);
+	body.image = imageUrl.url;
 	const result = await User.addUser(body);
 	const response = responseFormatter(OK, SUCCESS, result);
 	responseCacher(req, res, response);
@@ -28,6 +39,9 @@ exports.postUserController = tryCatch(async (req, res) => {
 
 exports.patchUserController = tryCatch(async (req, res) => {
 	const { body } = req;
+	const { image } = body;
+	const imageUrl = await v2.uploader.upload(image);
+	body.image = imageUrl.url;
 	const result = await User.updateUser(body);
 	let response;
 	if ('firstName' in result) {
